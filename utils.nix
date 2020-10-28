8831b51a7587fc7a8059a6b919cc29ac9d0e0e38
Silvan Mosberger <contact@infinisil.com>
2020-10-28 15:31:14 +0100

    Fix lookupNixPath missing /

--- utils.nix
+++ utils.nix
@@ -6,5 +6,5 @@
       entry = lib.findFirst (e: lib.hasPrefix e.prefix path)
         (throw "No entry matching ${toString path} found in NIX_PATH") builtins.nixPath;
       suffix = lib.removePrefix entry.prefix path;
-    in entry.path + suffix;
+    in entry.path + "/${suffix}";
 }
