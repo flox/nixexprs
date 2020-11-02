c25159ad83030388c8f4f1ff8100576d41f56eb8
Silvan Mosberger <contact@infinisil.com>
2020-11-02 14:56:37 +0100

    Also auto-call files

--- auto/call.nix
+++ auto/call.nix
@@ -6,9 +6,16 @@
 , withVerbosity
 }:
 let
-  # TODO: Warn or error or do something else for non-directories?
-  subdirs = lib.attrNames (lib.filterAttrs (name: type: type == "directory") (builtins.readDir path));
-  subdirPackage = name: withVerbosity 4
-    (builtins.trace "Auto-calling ${toString (path + "/${name}")}")
-    (callPackage (path + "/${name}") {});
-in lib.genAttrs subdirs subdirPackage
+  subpaths = lib.mapAttrs' (name: type:
+    {
+      regular =
+        if lib.hasSuffix ".nix" name
+        then lib.nameValuePair (lib.removeSuffix ".nix" name) (path + "/${name}")
+        else throw "Can't auto-call non-Nix file ${toString (path + "/${name}")}. "
+          + "If non-Nix files are needed for a package, move the package into its own directory and use a default.nix file for the Nix expression";
+      directory = lib.nameValuePair name (path + "/${name}");
+    }.${type} or (throw "Can't auto-call file type ${type}")) (builtins.readDir path);
+  subpathPackage = name: path: withVerbosity 4
+    (builtins.trace "Auto-calling ${toString path}")
+    (callPackage path {});
+in lib.mapAttrs subpathPackage subpaths
