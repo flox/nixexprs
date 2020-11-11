flox/nixexprs

commit 8e48a54ec4bb23e310aa2ed00b05982919e75182

Only import channels from NIX_PATH that are flox channels
So that e.g. channels.nixpkgs doesn't try to import nixpkgs as a flox
channel

channel/default.nix
@@ -11,6 +11,8 @@
 , return ? "outputs"
 , srcpath ? ""
 , system ? builtins.currentSystem
+# Used to detect whether this default.nix is a channel (by inspecting function arguments)
+, isFloxChannel ? throw "This argument isn't meant to be accessed"
 }@args:
 let
   topdir' = topdir;
@@ -84,26 +86,35 @@ let
     inherit name requiresImportingChannelArgs topdir extraOverlays args;
   };
 
-  # Mapping from channel name to a path to its nixexprs root
+  # Mapping from channel name to the value at its default.nix
   # Search through both prefixed and non-prefixed paths in NIX_PATH
   channelNixexprs =
     let
       expandEntry = e:
         if e.prefix != "" then [{
           name = e.prefix;
-          value = e.path;
+          path = e.path;
         }]
         # TODO: Change flox wrapper to do this expansion
         else lib.mapAttrsToList (name: type: {
           inherit name;
-          value = e.path + "/${name}";
+          path = e.path + "/${name}";
         }) (builtins.readDir e.path);
-      result = lib.listToAttrs (lib.concatMap expandEntry builtins.nixPath);
+
+      pathEntries = lib.concatMap expandEntry builtins.nixPath;
+      exprEntries = map (e: e // { value = import e.path; }) (lib.filter (e: builtins.pathExists (e.path + "/default.nix")) pathEntries);
+      channelEntries = lib.filter (e:
+        let
+          isFloxChannel = lib.isFunction e.value && (lib.functionArgs e.value) ? isFloxChannel;
+          result = if isFloxChannel
+            then withVerbosity 1 (builtins.trace "[channel ${e.name}] Importing from `${toString e.path}`") isFloxChannel
+            else withVerbosity 3 (builtins.trace "NIX_PATH entry ${e.name} points to path ${e.path} which is not a flox channel (${lib.generators.toPretty {} e.value}), ignoring this entry") isFloxChannel;
+        in result
+      ) exprEntries;
+      result = lib.listToAttrs channelEntries;
     in withVerbosity 1 (builtins.trace "Found these channel-like entries in NIX_PATH: ${toString (lib.attrNames result)}") result;
 
-  importChannelSrc = name: src: withVerbosity 1
-    (builtins.trace "[channel ${name}] Importing from `${toString src}`")
-    (import src { inherit name; return = "channelArguments"; });
+  importChannelSrc = name: fun: fun { inherit name; return = "channelArguments"; };
 
   channelArgs = lib.mapAttrs importChannelSrc channelNixexprs // {
     ${name} = myChannelArgs;
@@ -135,7 +146,7 @@ let
     channelPkgs {} args
   ) channelArgs;
 
-  channels = lib.mapAttrs (_: importingChannelArgs:
+  channels = lib.mapAttrs (parentName: importingChannelArgs:
     lib.mapAttrs (name: args:
       # If the channel to import doesn't require access to the importing channel,
       # don't reimport it again, just use the shared one from the independentChannel mapping

commit 924a254cd35a1d8ac7e888442aba15ce8a847cce

Fix nixPath name inference broken
This was broken in 8e48a54ec4bb23e310aa2ed00b05982919e75182

channel/default.nix
@@ -53,8 +53,8 @@ let
         gitConfig = import ./nameFromGit.nix { inherit lib topdir; };
         nixPath =
           let
-            matchingEntries = lib.filterAttrs (name: path: path == topdir) channelNixexprs;
-            matchingNames = lib.unique (lib.attrNames matchingEntries);
+            matchingEntries = lib.filter (e: e.path == topdir) channelNixexprsList;
+            matchingNames = lib.unique (map (e: e.name) matchingEntries);
           in if lib.length matchingNames == 0 then { failure = "No entries in NIX_PATH match path ${topdir}"; }
           else if lib.length matchingNames == 1 then { success = lib.elemAt matchingNames 0; }
           else { failure = "Multiple entries in NIX_PATH match path ${topdir}"; };
@@ -86,9 +86,9 @@ let
     inherit name requiresImportingChannelArgs topdir extraOverlays args;
   };
 
-  # Mapping from channel name to the value at its default.nix
-  # Search through both prefixed and non-prefixed paths in NIX_PATH
-  channelNixexprs =
+  # List of { name, path, value } entries of channels found in NIX_PATH
+  # Searches through both prefixed and non-prefixed paths in NIX_PATH
+  channelNixexprsList =
     let
       expandEntry = e:
         if e.prefix != "" then [{
@@ -111,8 +111,9 @@ let
             else withVerbosity 3 (builtins.trace "NIX_PATH entry ${e.name} points to path ${e.path} which is not a flox channel (${lib.generators.toPretty {} e.value}), ignoring this entry") isFloxChannel;
         in result
       ) exprEntries;
-      result = lib.listToAttrs channelEntries;
-    in withVerbosity 1 (builtins.trace "Found these channel-like entries in NIX_PATH: ${toString (lib.attrNames result)}") result;
+    in withVerbosity 1 (builtins.trace "Found these channel-like entries in NIX_PATH: ${toString (map (e: e.name) channelEntries)}") channelEntries;
+
+  channelNixexprs = lib.listToAttrs channelNixexprsList;
 
   importChannelSrc = name: fun: fun { inherit name; return = "channelArguments"; };
 