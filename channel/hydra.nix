7cba5ff15867527c4167fe85264b70ab196177d4
Silvan Mosberger <contact@infinisil.com>
2020-11-18 21:25:16 +0100

    Allow passing supportedSystems in hydra.nix and add aarch by default

--- channel/hydra.nix
+++ channel/hydra.nix
@@ -1,9 +1,11 @@
-{ floxChannelName }:
+{ floxChannelName
+, supportedSystems ? [ "x86_64-linux" "aarch64-linux" ]
+}:
 let
   channelRoot = builtins.findFile builtins.nixPath floxChannelName;
   channel = import channelRoot;
   releaseLib = import <nixpkgs/pkgs/top-level/release-lib.nix> {
-    supportedSystems = [ "x86_64-linux" ];
+    inherit supportedSystems;
     packageSet = channel;
   };
   channelOutputs = channel {};
