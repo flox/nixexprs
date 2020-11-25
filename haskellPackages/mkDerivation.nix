4d7476f474c23196a54c91ea7c876dc7546d0e0d
Silvan Mosberger <contact@infinisil.com>
2020-11-25 20:15:26 +0100

    Make Haskell builder embed .flox.json in $out

--- haskellPackages/mkDerivation.nix
+++ haskellPackages/mkDerivation.nix
@@ -1,10 +1,21 @@
-{ mkDerivation, meta }:
+{ mkDerivation, lib, meta }:
 
 { project	# the name of the project, required
 , ... } @ args:
 
+let
+  source = meta.getBuilderSource project args;
+in
 mkDerivation (removeAttrs args [ "project" ] // {
-  inherit (meta.getBuilderSource project args) pname version src;
+  inherit (source) pname version src;
+
+  # Create .flox.json file in root of package dir to record
+  # details of package inputs.
+  postInstall = toString (args.postInstall or "") + ''
+    mkdir -p $out
+    echo ${lib.escapeShellArg source.src_json} > $out/.flox.json
+  '';
+
   passthru = { inherit project; } // args.passthru or {};
   license = args.license or null;
 })
