e02f45e044310c27b6556cc320c40a3c84acd639
jD91mZM2 <me@krake.one>
2021-01-20 16:12:46 +0100

    Copy rust tests

---
+++ tests/naerskBuildRustPackage/config.nix
@@ -0,0 +1,22 @@
+{ jq, nixpkgs, repo }: {
+  type = "build";
+  exitCode = 0;
+  nixPath = [
+    {
+      prefix = "nixpkgs";
+      path = nixpkgs;
+    }
+    {
+      prefix = "flox";
+      path = repo;
+    }
+    {
+      prefix = "";
+      path = ./channels;
+    }
+  ];
+  postCommands = [
+    "result/bin/hello"
+    "${jq}/bin/jq . result/.flox.json"
+  ];
+}
