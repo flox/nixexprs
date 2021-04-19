ea94375f7110b0b9fa2f60607ba636830cb97e7a
Michael Brantley <brantley@deshaw.com>
2021-04-19 08:08:33 -0400

    channel/hydra.nix: allow nixpkgsArgs
    Allow specification of nixpkgsArgs by way of the Hydra configuration.

--- channel/hydra.nix
+++ channel/hydra.nix
@@ -1,14 +1,16 @@
-{ floxChannelName, supportedSystems ? [ "x86_64-linux" "aarch64-linux" ] }:
+{ floxChannelName, supportedSystems ? [ "x86_64-linux" "aarch64-linux" ]
+, nixpkgsArgs ? {
+  config = {
+    allowUnfree = false;
+    inHydra = true;
+  };
+} }:
 let
   channelRoot = builtins.findFile builtins.nixPath floxChannelName;
   channel = import channelRoot;
   releaseLib = import <nixpkgs/pkgs/top-level/release-lib.nix> {
     inherit supportedSystems;
     packageSet = channel;
-    nixpkgsArgs = {
-      config.allowUnfree = false;
-      config.inHydra = true;
-      name = floxChannelName;
-    };
+    nixpkgsArgs = nixpkgsArgs // { name = floxChannelName; };
   };
 in releaseLib.mapTestOn (releaseLib.packagePlatforms releaseLib.pkgs)
