db4f05cdf113c2152238dd8a72d35591e5ac5a35
Silvan Mosberger <contact@infinisil.com>
2021-06-07 22:52:26 +0200

    Introduce meta.scope
    Allows access to the used scope for the current package

--- channel/output.nix
+++ channel/output.nix
@@ -280,7 +280,11 @@ let
   createSet = spec: super:
     lib.mapAttrs (pname: value:
       let
-        localMeta = meta // { inherit (spec) channels; };
+        localMeta = meta // {
+          inherit (spec) channels;
+
+          inherit scope;
+        };
 
         # TODO: Probably more efficient to directly inspect function arguments and fill these entries out.
         # A callPackage abstraction that allows specifying multiple attribute sets might be nice
