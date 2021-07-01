5795aafbfd4a25b3c387cb962abb4bb4c1b7ffde
Silvan Mosberger <contact@infinisil.com>
2021-07-01 16:11:37 +0200

    WIP

--- channel/default.nix
+++ channel/default.nix
@@ -1,5 +1,13 @@
 # Arguments for the channel file in floxpkgs
-{ name ? null, topdir, extraOverlays ? [ ] }@chanArgs:
+{ name ? null
+, topdir
+# FIXME: Deprecation warning
+, extraOverlays ? null
+, dependencies ?
+  if builtins.pathExists (topdir + "/channels.json")
+  then builtins.fromJSON (builtins.readFile (topdir + "/channels.json"))
+  else []
+}@chanArgs:
 
 # Arguments for the command line
 { name ? null, debugVerbosity ? 0
@@ -43,7 +51,7 @@ in let
   nameHeuristics = let
     f = name: value:
       let
-        result = if value ? success then
+        result = /*if value ? success then
           let
             # Find a channel mapping in NIX_PATH that matches the name
             # This is case-insensitive because GitHub usernames are as well
@@ -57,7 +65,7 @@ in let
               "Inferred channel name ${value.success} using heuristic ${name}, but no entry for this channel found in NIX_PATH"
               value.success;
           }
-        else
+        else*/
           value;
       in result // { inherit name; };
 
@@ -80,21 +88,21 @@ in let
         failure = ''Directory name of topdir is just "floxpkgs"'';
       };
       gitConfig = import ./nameFromGit.nix { inherit lib topdir; };
-      nixPath = let
-        matchingEntries = lib.filter (e: e.path == topdir) channelFloxpkgsList;
-        matchingNames = lib.unique (map (e: e.name) matchingEntries);
-      in if lib.length matchingNames == 0 then {
-        failure = "No entries in NIX_PATH match path ${topdir}";
-      } else if lib.length matchingNames == 1 then {
-        success = lib.elemAt matchingNames 0;
-      } else {
-        failure = "Multiple entries in NIX_PATH match path ${topdir}";
-      };
+      #nixPath = let
+      #  matchingEntries = lib.filter (e: e.path == topdir) channelFloxpkgsList;
+      #  matchingNames = lib.unique (map (e: e.name) matchingEntries);
+      #in if lib.length matchingNames == 0 then {
+      #  failure = "No entries in NIX_PATH match path ${topdir}";
+      #} else if lib.length matchingNames == 1 then {
+      #  success = lib.elemAt matchingNames 0;
+      #} else {
+      #  failure = "Multiple entries in NIX_PATH match path ${topdir}";
+      #};
     };
     ordered = [
       heuristics.chanArgs
       heuristics.cmdArgs
-      heuristics.nixPath
+      #heuristics.nixPath
       heuristics.baseName
       heuristics.gitConfig
     ];
@@ -120,74 +128,174 @@ in let
   firstSuccess.success;
 
   myChannelArgs = {
-    inherit name topdir extraOverlays;
+    inherit name topdir extraOverlays dependencies;
     inherit _floxPathDepth;
   };
 
-  # List of { name, path, value } entries of channels found in NIX_PATH
-  # Searches through both prefixed and non-prefixed paths in NIX_PATH
-  #
-  # See https://github.com/flox/floxpkgs/blob/staging/docs/expl/channel-discovery.md
-  # for an explanation of why channels are discovered through NIX_PATH
-  channelFloxpkgsList = let
-    expandEntry = e:
-      if e.prefix != "" then [{
-        name = e.prefix;
-        path = e.path;
-      }]
-      # TODO: Change flox wrapper to do this expansion
-      else
-        lib.mapAttrsToList (name: type: {
-          inherit name;
-          path = e.path + "/${name}";
-        }) (builtins.readDir e.path);
-
-    pathEntries = lib.concatMap expandEntry builtins.nixPath;
-    exprEntries = map (e: e // { value = import e.path; })
-      (lib.filter (e: builtins.pathExists (e.path + "/default.nix"))
-        pathEntries);
-    channelEntries = lib.filter (e:
-      let
-        isFloxChannel = builtins.tryEval (lib.isFunction e.value
-          && (lib.functionArgs e.value) ? _isFloxChannel);
-        result = if isFloxChannel.success then
-          if isFloxChannel.value then
-            withVerbosity 1 (builtins.trace
-              "[channel ${e.name}] Importing from `${toString e.path}`") true
-          else
-            withVerbosity 3 (builtins.trace
-              "NIX_PATH entry ${e.name} points to path ${e.path} which is not a flox channel (${
-                lib.generators.toPretty { } e.value
-              }), ignoring this entry") false
-        else
-          withVerbosity 3 (builtins.trace
-            "NIX_PATH entry ${e.name} points to a path ${e.path} which can't be evaluated successfully, ignoring this entry")
-          false;
-      in result) exprEntries;
-  in withVerbosity 1 (builtins.trace
-    "Found these channel-like entries in NIX_PATH: ${
-      toString (map (e: e.name) channelEntries)
-    }") channelEntries;
-
-  channelFloxpkgs = lib.listToAttrs channelFloxpkgsList;
-
-  importChannelSrc = name: fun:
-    fun {
+  closure = builtins.genericClosure {
+    startSet = [{
+      key = name;
       inherit name;
-      _return = "channelArguments";
+      value = myChannelArgs;
+    }];
+    operator = entry: map (name: {
+      key = name;
+      inherit name;
+      value = import (builtins.findFile builtins.nixPath name) {
+        inherit name;
+        _return = "channelArguments";
+      };
+    }) entry.value.dependencies;
+  };
+
+  dependencyGraph = pkgs.runCommandNoCC "floxpkgs-${name}-dependency-graph" {
+    graph = ''
+      digraph {
+        "${name}" [shape=box];
+      ${lib.concatMapStrings (entry:
+        lib.concatMapStrings (dep:
+          "  \"${entry.name}\" -> \"${dep}\";\n"
+        ) entry.value.dependencies
+      ) closure}}
+    '';
+    passAsFile = [ "graph" ];
+    nativeBuildInputs = [ pkgs.graphviz ];
+  } ''
+    dot -Tpng "$graphPath" -Gdpi=300 -o $out
+  '';
+
+  allChannels = builtins.listToAttrs closure;
+
+  #importChannelSrc = name: fun:
+  #  fun {
+  #    inherit name;
+  #    _return = "channelArguments";
+  #  };
+
+  #channelArgs = lib.mapAttrs importChannelSrc channelFloxpkgs // {
+  #  ${name} = myChannelArgs;
+  #};
+
+  #outputFun = import ./output.nix {
+  #  inherit outputFun channelArgs pkgs withVerbosity;
+  #  sourceOverrides = builtins.fromJSON sourceOverrideJson;
+  #};
+
+  # Turns a directory into an attribute set.
+  # Files with a .nix suffix get turned into an attribute name without the
+  # suffix. Directories get turned into an attribute of their name directly.
+  # If there is both a .nix file and a directory with the same name, the file
+  # takes precedence. The context argument is a string shown in trace messages
+  # Each value in the resulting attribute sets has attributes
+  # - value: The Nix value of the file or of the default.nix file in the directory
+  # - deep: In case of directories, whether there is a deep-override file within it. For files always false
+  # - path: The path to the Nix directory/file that was imported
+  # - type: The file type, either "regular" for files or "directory" for directories
+  dirToAttrs = context: dir:
+    let
+      exists = builtins.pathExists dir;
+
+      importPath = name: type:
+        let path = dir + "/${name}";
+        in {
+          directory = lib.nameValuePair name {
+            deep = builtins.pathExists (path + "/deep-override");
+            inherit path type;
+          };
+
+          regular = if lib.hasSuffix ".nix" name then
+            lib.nameValuePair (lib.removeSuffix ".nix" name) {
+              deep = false;
+              inherit path type;
+            }
+          else
+            null;
+        }.${type} or (throw "Can't auto-call file type ${type} at ${toString path}");
+
+      # Mapping from <package name> -> { value = <package fun>; deep = <bool>; }
+      # This caches the imports of the auto-called package files, such that they don't need to be imported for every version separately
+      entries = lib.filter (v: v != null)
+        (lib.attrValues (lib.mapAttrs importPath (builtins.readDir dir)));
+
+      # Regular files should be preferred over directories, so that e.g.
+      # foo.nix can be used to declare a further import of the foo directory
+      entryAttrs =
+        lib.listToAttrs (lib.sort (a: b: a.value.type == "regular") entries);
+
+      message = ''
+        ${context} Importing all Nix expressions from directory "${
+          toString dir
+        }"'' + withVerbosity 6
+        (_: ". Attributes: ${toString (lib.attrNames entryAttrs)}") "";
+
+      result = if exists then
+        withVerbosity 4 (builtins.trace message) entryAttrs
+      else
+        withVerbosity 5 (builtins.trace
+          "${context} Not importing any Nix expressions because `${
+            toString dir
+          }` does not exist") { };
+
+    in result;
+
+  /* Imports all directories and Nix files of the given package directory subpath. Returns
+      {
+        # For attributes that have { deep = true; } in their package directory (doesn't work for files)
+        deep = {
+          <name> = <value>;
+        };
+        # For attributes that don't have { deep = true; }
+        shallow = {
+          <name> = <value>;
+        };
+      }
+     See dirToAttrs for the fields of <value>
+  */
+  packageSetFuns = setName: subpath:
+    let
+      #dir = myArgs.topdir + "/${subpath}";
+
+      #entries = lib.mapAttrsToList lib.nameValuePair
+      #  (dirToAttrs "packageSet ${setName}" dir);
+
+      # { <channel> = { <name> = <entry>; }; }
+      #entries = lib.mapAttrs (name: args:
+      #  dirToAttrs "[channel ${name}] [packageSet ${setName}]" args.topdir
+      #) allChannels;
+      # flip
+      # => { <name> = { <channel> = <entry>; }; }
+      # resolve
+      # => { <name> = <entry>; }
+      # deep-partition
+
+      # [ { channel = ...; name = ...; value = <entry>; } ]
+      entries = lib.concatMap (entry:
+        lib.mapAttrsToList (name: value: {
+          channel = entry.name;
+          inherit name;
+          value = value // {
+            channel = entry.name;
+          };
+        }) (dirToAttrs "[channel ${name}] [packageSet ${setName}]" (entry.value.topdir + "/${subpath}"))
+      ) closure;
+
+      resolve = name: entries: if lib.length entries > 1 then throw "You need to resolve entry ${name}" else builtins.trace "No need to resolve entry ${name}, it only occurs in ${(lib.head entries).channel}" (lib.head entries);
+
+      # { <name> = <entry>; }
+      resolved = lib.mapAttrsToList resolve (lib.groupBy (entry: entry.name) entries);
+
+
+      parts = lib.mapAttrs (n: v: lib.listToAttrs (map (e: e // { value = { inherit (e.value) channel path; }; }) v))
+        (lib.partition (e: e.value.deep) resolved);
+
+    in {
+      deep = parts.right;
+      shallow = parts.wrong;
     };
-
-  channelArgs = lib.mapAttrs importChannelSrc channelFloxpkgs // {
-    ${name} = myChannelArgs;
-  };
-
-  outputFun = import ./output.nix {
-    inherit outputFun channelArgs pkgs withVerbosity;
-    sourceOverrides = builtins.fromJSON sourceOverrideJson;
-  };
 
   # Evaluate name early so that name inference warnings get displayed at the start, and not just once we depend on another channel
 in builtins.seq name {
-  outputs = outputFun [ ] myChannelArgs myChannelArgs;
+  outputs = packageSetFuns "toplevel" "pkgs";
+  inherit dependencyGraph;
   channelArguments = myChannelArgs;
 }.${_return}
